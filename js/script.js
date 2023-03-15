3d95d0c1cafd9e11d82214ed2e9ea77beea2f90c
josel0 <gothangel21@hotmail.com>
2023-03-14 21:16:00 -0500

    1 commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".course-slider", {
+    loop: true,
+    grabCursor: true,
+    // spaceBetween: 20,
+    // centeredSlides: true,
+    // slidesPerView: 1,
+    spaceBetween: 10,
+
+    pagination: {
+      el: ".swiper-pagination",
+    clickable: true, 
+    },
+    breakpoints: {
+        550: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+      keyboard: {
+        enabled: true,
+      },
+  });
