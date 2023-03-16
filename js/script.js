1a79864157eb7cc833455a484b15cbb830b8a467
josel0 <gothangel21@hotmail.com>
2023-03-16 14:08:43 -0500

    10 commit

--- js/script.js
+++ js/script.js
@@ -49,3 +49,21 @@ var swiper = new Swiper(".course-slider", {
 		disableOnInteraction: false, //vuelva a lo mismo
 	},
 });
+
+// panel de busqueda
+function buscarPelicula() {
+	let input = document.getElementById("buscador");
+	let filter = input.value.toUpperCase();
+	let divs = document.getElementById("peliculas").getElementsByTagName("div");
+
+	for (let i = 0; i < divs.length; i++) {
+		let pelicula = divs[i].getElementsByTagName("p")[0];
+		let titulo = pelicula.textContent || pelicula.innerText;
+
+		if (titulo.toUpperCase().indexOf(filter) > -1) {
+			divs[i].style.display = "";
+		} else {
+			divs[i].style.display = "none";
+		}
+	}
+}
