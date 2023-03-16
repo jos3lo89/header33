4e333a5d9bad5a8a46d5f399fe6176cd2f8e9342
josel0 <gothangel21@hotmail.com>
2023-03-16 10:09:26 -0500

    commit del script

--- js/script.js
+++ js/script.js
@@ -1,38 +1,46 @@
 // header start
-let menuBtn = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .flex .navbar');
+let menuBtn = document.querySelector("#menu-btn");
+let navBar = document.querySelector(".header .flex .navbar");
 
-menuBtn.onclick = () => {
-    menuBtn.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+menuBtn.addEventListener("click", () => {
+	navBar.classList.toggle("active");
+	menuBtn.classList.toggle("fa-times");
+});
+
+document.addEventListener("click", (event) => {
+	if (event.target.closest(".header")) {
+		return;
+	}
+	navBar.classList.remove("active");
+	menuBtn.classList.remove("fa-times");
+});
 // header end
 
 // swiper js
 var swiper = new Swiper(".course-slider", {
-    loop: true,
-    // grabCursor: true,
-    // spaceBetween: 20,
-    // centeredSlides: true,
-    // slidesPerView: 1,
-    spaceBetween: 10,
+	loop: true,
+	// grabCursor: true,
+	// spaceBetween: 20,
+	// centeredSlides: true,
+	// slidesPerView: 1,
+	spaceBetween: 10,
 
-    pagination: {
-      el: ".swiper-pagination",
-    clickable: true, 
-    },
-    breakpoints: {
-        550: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
-      },
-      keyboard: {
-        enabled: true,
-      },
-  });
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		550: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		1024: {
+			slidesPerView: 4,
+		},
+	},
+	keyboard: {
+		enabled: true,
+	},
+});
