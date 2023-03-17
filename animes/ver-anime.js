3e584ca7aa3d5f9d0e3833a61f439de55ce3fe50
josel0 <gothangel21@hotmail.com>
2023-03-17 11:16:05 -0500

    12 commit cambio de colores y reproductor

--- animes/ver-anime.js
+++ animes/ver-anime.js
@@ -15,3 +15,38 @@ document.addEventListener("click", (event) => {
 	menuBtn.classList.remove("fa-times");
 });
 // header end
+
+// Obtener la lista de capítulos
+const chapterList = document.querySelector(".chapter-list");
+
+// Obtener el iframe del reproductor de video
+const videoIframe = document.getElementById("video-iframe");
+
+// Agregar un controlador de eventos clic a cada elemento li de la lista de capítulos
+chapterList.querySelectorAll("li").forEach((chapter) => {
+	chapter.addEventListener("click", () => {
+		// Obtener el src del video correspondiente al capítulo seleccionado
+		const videoSrc = chapter.getAttribute("data-src");
+
+		// Actualizar el src del iframe del reproductor de video
+		videoIframe.src = videoSrc;
+
+		// Agregar la clase 'active' al elemento li que se hizo clic
+		chapterList.querySelectorAll("li").forEach((li) => {
+			li.classList.remove("active");
+		});
+
+		chapter.classList.add("active");
+	});
+
+	// Agregar un controlador de eventos hover al elemento li
+	chapter.addEventListener("mouseover", () => {
+		// Cambiar el color de fondo del elemento li cuando se pasa el mouse sobre él
+		chapter.classList.add("hover");
+	});
+
+	chapter.addEventListener("mouseout", () => {
+		// Cambiar el color de fondo del elemento li cuando se deja de pasar el mouse sobre él
+		chapter.classList.remove("hover");
+	});
+});
